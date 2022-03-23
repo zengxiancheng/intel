dea9de516a97302f769aa5ca3f48b301e7b94f38
zengxcphoto <zengxcphoto@163.com>
2022-03-23 13:26:07 +0800

    测试提交

--- src/main/java/com/cn/china/controller/TestController.java
+++ src/main/java/com/cn/china/controller/TestController.java
@@ -1,10 +1,16 @@
 package com.cn.china.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 //测试提交
 @Controller
 public class TestController {
+	
+	@RequestMapping
+	public String test(){
+		return "hello";
+	}
 
 }
